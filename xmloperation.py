f1c5d43052161ddbcf3a62d0253632945e25c10c
iammidhun <midhunmohanputhur@gmail.com>
2020-03-03 07:58:00 +0530

    converting the value of address nodes to address

--- xmloperation.py
+++ xmloperation.py
@@ -1,4 +1,10 @@
 from xml.dom import minidom
 xmldoc = minidom.parse('Downloads/sample.xml')
+
 #getting elements of address
 addr_elements = xmldoc.getElementsByTagName('addr')
+#to convert address value 
+for items in addr_elements:
+    for elements in items.childNodes :
+        if elements.childNodes:
+            elements.childNodes[0].data = "address"
