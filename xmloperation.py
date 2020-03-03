a955b63a6b39fafee4efc221f2eb951fe09a805c
iammidhun <midhunmohanputhur@gmail.com>
2020-03-03 08:17:30 +0530

    converting xml file to json file

--- xmloperation.py
+++ xmloperation.py
@@ -1,3 +1,5 @@
+import xmltodict
+import json
 from xml.dom import minidom
 xmldoc = minidom.parse('sample.xml')
 
@@ -15,9 +17,16 @@ for items in family_elements:
     elements = items.childNodes
     elements[0].data = "Patient Family Name"
 #to convert patient name
-person_elementss = xmldoc.getElementsByTagName('given')
-for items in action_code:
-    elements = i.childNodes
+person_elements = xmldoc.getElementsByTagName('given')
+for items in person_elements:
+    elements = items.childNodes
     elements[0].data = "Patient Name"
+#writing xml file 
+with open('output.xml','w+') as ifile:
+    ifile.write(xmldoc.toprettyxml())
 
 
+with open('output.xml') as in_file:
+    xml = in_file.read()
+    with open('jsondata.json', 'w') as out_file:
+        json.dump(xmltodict.parse(xml), out_file)
